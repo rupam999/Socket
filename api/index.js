c85712d59242e7ea02acba0c404e74e4df295fa0
rupamc <rupam.chakraborty@exa-ag.com>
2022-09-08 13:04:33 +0530

    Vercel try

--- api/index.js
+++ api/index.js
@@ -1,35 +1,37 @@
-const express = require("express")
-const { Server } = require("socket.io");
+const express = require('express');
+const { Server } = require('socket.io');
 var http = require('http');
-const cors = require("cors")
+const cors = require('cors');
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 
 var server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
-  }
+	cors: {
+		origin: 'http://localhost:3000',
+		methods: ['GET', 'POST'],
+	},
 });
 
-app.get("/", (req, res) => {res.send("Chat BE with Socket.io by Prince Raj"); res.end()})
+app.get('/api', (req, res) => {
+	io.on('connection', (socket) => {
+		console.log(socket.id);
 
-io.on("connection", (socket) => {
-  console.log(socket.id)
+		socket.on('joinRoom', (room) => {
+			socket.join(room);
+		});
 
-  socket.on("joinRoom", room => {
-		socket.join(room)
-  })
+		socket.on('newMessage', ({ newMessage, room }) => {
+			io.in(room).emit('getLatestMessage', newMessage);
+		});
+	});
 
-  socket.on("newMessage", ({newMessage, room}) => {
-    io.in(room).emit("getLatestMessage", newMessage)
-  })
-
+	res.send('Chat BE with Socket.io by Prince Raj');
+	res.end();
 });
 
-const port = process.env.PORT || 9000
+const port = process.env.PORT || 9000;
 
-server.listen(port, console.log(`App started at port ${port}`))
+server.listen(port, console.log(`App started at port ${port}`));
